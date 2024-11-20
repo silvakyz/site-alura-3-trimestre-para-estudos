046a4fdc20213b9c07b9b3d51ffbafe2a3d11b86
silvakyz <141843608+silvakyz@users.noreply.github.com>
2024-11-20 19:43:51 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação',
+    'O que é C#?',
+    'O C# é uma linguagem de programação de jogos'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da Geórgia?',
+    'A capital da Geórgia é Tiblissi'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma Arrays?',
+    'Arrays é um paradigma de programação baseado em objetos, que combinam dados (atributos) e comportamentos (métodos).'
+)
+
+criaCartao(
+    'Lingua Esperanto',
+    'Como se diz Tudo bem em Esperanto?',
+    'Tudo bem em Esperanto é Ĉu ĉio estas en ordo? (lê-se: "Chu chio estas en ordo?").'
+)
